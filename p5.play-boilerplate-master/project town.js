58b554c170645537f29ff76ac2671ca4bf9694e0
VashishthSrivastava <65722220+VashishthSrivastava@users.noreply.github.com>
2020-07-15 19:44:23 +0530

    Add files via upload

---
+++ p5.play-boilerplate-master/project town.js
@@ -0,0 +1,79 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;    
+var hexagon;
+var object;
+
+
+function setup() {
+
+  createCanvas(800,400);
+  createSprite(400, 200, 50, 50);
+    engine = Engine.create();
+    world = engine.world;
+
+    ground = new Ground(600,height,1200,20);
+    platform = new Ground(150, 305, 300, 170)
+
+    //level one
+    block1 = new Block(330,235,30,40);
+    block2 = new Block(330,235,30,40);
+    block3 = new Block(330,235,30,40);
+    block4 = new Block(330,235,30,40);
+    block5 = new Block(330,235,30,40);
+    block6 = new Block(330,235,30,40);
+    block7 = new Block(330,235,30,40);
+    //level two
+    block8  = new Block(330,235,30,40);
+    block9  = new Block(360,235,30,40);
+    block10 = new Block(390,235,30,40);
+    block11 = new Block(420,235,30,40);
+    block12 = new Block(450,235,30,40);
+    //level three
+    block13 = new Block(360,195,30,40);
+    block14 = new Block(390,195,30,40);
+    block15 = new Block(420,195,30,40);
+    //top
+    block16 = new Block(390,155,30,40);
+
+    //polygon holder with slings
+    polygon = Bodies.circle(50,200,20);
+    Worls.add(world,polygon);
+
+    SlingShot = new SlingShot(this.polygon,{x:100,y:200 });
+
+}
+
+function draw() {
+  background(255,255,255);  
+  drawSprites();
+
+  block8.display();
+  block9.display();
+  block10.display();
+  block11.display();
+  block12.display();
+  block13.display();
+  block14.display();
+  block15.display();
+  block16.display();
+  SlingShot.display();
+
+
+
+
+
+
+}
+
+function mouseDragged(){
+  Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
+}
+
+
+function mouseReleased(){
+  slingshot.fly();
+}
